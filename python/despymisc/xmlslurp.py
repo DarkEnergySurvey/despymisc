5e6a602a7391bf5cd3450bd804cc62b93c2c76a7
Doug Friedel <friedel@illinois.edu>
2019-09-12 13:14:56 -0500

    fixed formatting and made the code case agnostic

--- python/despymisc/xmlslurp.py
+++ python/despymisc/xmlslurp.py
@@ -6,11 +6,11 @@ class Xmlslurper:
 
     def __init__(self, filename, tablenames):
         self.data = {}
-        
+
 
         ##################################################################
-        def start_element(name, attrs, data = self.data):
-            if "TABLE" == name:
+        def start_element(name, attrs, data=self.data):
+            if 'TABLE' == name.upper():
                 # skip if not one of the desired tables
                 if not attrs['name'] in self.data['wanted_tables']:
                     return
@@ -22,46 +22,46 @@ class Xmlslurper:
                 data['fieldarray'] = []
                 data['tables'][data['curtable']] = []
 
-            if "FIELD" == name and self.data['curtable']:
+            if 'FIELD' == name.upper() and self.data['curtable']:
                 # save description information
                 data['fieldnames'].append(attrs['name'].lower())
                 data['fieldtypes'].append(attrs['datatype'])
-                data['fieldarray'].append(attrs.get('arraysize',None))
+                data['fieldarray'].append(attrs.get('arraysize', None))
 
-            if "TR" == name:
+            if 'TR' == name.upper():
                 # new row, inialize row values
                 data['col'] = 0             # current column
-                data['prevcol'] = 0         # previous column 
+                data['prevcol'] = 0         # previous column
                 data['prevtext'] = ''       # previous text parsed in case partial due to buffer
                 data['currow'] = {}         # dictionary to store info from row
 
-            if "TD" == name:
+            if 'TD' == name.upper():
                 # save state that are in a TD section
                 data['in_TD'] = True
 
 
         ##################################################################
-        def end_element(name, data = self.data):
+        def end_element(name, data=self.data):
 
-            if "TD" == name:
+            if 'TD' == name.upper():
                 # if closed TD section, change TD state
                 data['col'] += 1
                 data['in_TD'] = False
 
-            if "TR" == name and data['curtable']:
+            if 'TR' == name.upper() and data['curtable']:
                 # save current row dictionary to current table
                 data['tables'][data['curtable']].append(data['currow'])
 
-            if "TABLE" == name and data['curtable']:
+            if 'TABLE' == name.upper() and data['curtable']:
                 # empty table variables
                 data['curtable'] = None
-                del self.data['fieldnames'] 
+                del self.data['fieldnames']
                 del self.data['fieldtypes']
                 del self.data['fieldarray']
 
 
         ##################################################################
-        def char_data(text, data = self.data):
+        def char_data(text, data=self.data):
             prevtext = text
 
             if data['in_TD'] and self.data['curtable']:
@@ -76,18 +76,18 @@ class Xmlslurper:
                 if curarrsize != None and curtype != 'char':
                     # data is for an array field
                     # assumes array cannot be of strings
-                
+
                     # so split into separate values
                     vals = text.strip().split()
 
                     # convert values to right type
                     if curtype == 'int':
-                        for i in range(0,len(vals)):
+                        for i in range(0, len(vals)):
                             vals[i] = int(vals[i])
                     elif curtype == 'float':
-                        for i in range(0,len(vals)):
+                        for i in range(0, len(vals)):
                             vals[i] = float(vals[i])
-                
+
                     # save data array to current row data
                     data['currow'][curname] = vals
                 else:
@@ -118,29 +118,29 @@ class Xmlslurper:
         self.data['curtable'] = None
         self.data['params'] = {}
 
-        # dictionary of tables 
+        # dictionary of tables
         #   tables are arrays of row dict
         self.data['tables'] = {}
         self.data['in_TD'] = False   # whether in TD section or not
 
         p = xml.parsers.expat.ParserCreate()
         #p.buffer_size=32768
-        p.buffer_size=2048
+        p.buffer_size = 2048
 
         # assign functions to handler
         p.StartElementHandler = start_element
         p.EndElementHandler = end_element
         p.CharacterDataHandler = char_data
 
-        f = open(filename,"r")
+        f = open(filename, "r")
         p.ParseFile(f)
         f.close()
 
         #
         # clean out our bookkeeping
         #
-        del self.data['curtable'] 
-        del self.data['wanted_tables'] 
+        del self.data['curtable']
+        del self.data['wanted_tables']
         del self.data['in_TD']
 
 
@@ -151,21 +151,20 @@ class Xmlslurper:
     #
     # we look like our data member...
     #
-    def __getattr__(self,blah):
+    def __getattr__(self, blah):
         return getattr(self.data['tables'], blah)
 
 
 
 
-if __name__ == "__main__":
-    tablelist = (
-            "Astrometric_Instruments",
-            "FGroups",
-            "Fields",
-            "Photometric_Instruments",
-            "PSF_Extensions",
-            "PSF_Fields",
-            "Warnings")
+if __name__ == "__main__":  # pragma no coverage
+    tablelist = ("Astrometric_Instruments",
+                 "FGroups",
+                 "Fields",
+                 "Photometric_Instruments",
+                 "PSF_Extensions",
+                 "PSF_Fields",
+                 "Warnings")
     import sys
     import glob
     import pprint
