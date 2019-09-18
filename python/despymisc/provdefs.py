0f4f60735b4e3e12613b7d6e821485b68c163650
Doug Friedel <friedel@illinois.edu>
2019-09-18 11:53:34 -0500

    pylint corrections

--- python/despymisc/provdefs.py
+++ python/despymisc/provdefs.py
@@ -2,10 +2,13 @@
 # $Rev:: 40841                            $:  # Revision of last commit.
 # $LastChangedBy:: mgower                 $:  # Author of last commit.
 # $LastChangedDate:: 2015-11-19 08:59:42 #$:  # Date of last commit.
+"""
+    Constants used in this module
+"""
 
 PROV_DELIM = ","
-PROV_USED  = "used"
-PROV_WGB   = "was_generated_by"
-PROV_WDF   = "was_derived_from"
+PROV_USED = "used"
+PROV_WGB = "was_generated_by"
+PROV_WDF = "was_derived_from"
 PROV_PARENTS = "parents"
 PROV_CHILDREN = "children"
