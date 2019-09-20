49e58b83ac1ce12407bd132e10f4cf3296a7d284
Doug Friedel <friedel@illinois.edu>
2019-09-20 12:42:27 -0500

    fleshed out the comments of each function

--- python/despymisc/miscutils.py
+++ python/despymisc/miscutils.py
@@ -18,7 +18,27 @@ import logging
 
 #######################################################################
 def fwdebug(msglvl, envdbgvar, msgstr, msgprefix=''):
-    """ print debugging message based upon thresholds """
+    """ Print debugging message based upon thresholds. If the environment
+        variable (envdbgvar) value is equal to or above msglvl, then print
+        the message. (see fwdebug_check for details)
+
+        Parameters
+        ----------
+        msglvl : int
+           The level of the message (the larger the number the less important
+           the massage is: e.g. 0 = critial, 10 = only print this if you want
+           to know in depth details for debugging)
+
+        envdbgvar : str
+           The environment variable to check the value of
+
+        msgstr : str
+            The message to print
+
+        msgprefix : str
+            A value to prepend to the message. Default is an empty string
+
+    """
     # environment debug variable overrides code set level
 
     if fwdebug_check(msglvl, envdbgvar):
@@ -26,7 +46,29 @@ def fwdebug(msglvl, envdbgvar, msgstr, msgprefix=''):
 
 #######################################################################
 def fwdebug_check(msglvl, envdbgvar):
-    """ print debugging message based upon thresholds """
+    """ Check whether the given msglvl is equal to or lower than the current
+        debug level. The debug level is determined by (in order of precidence):
+
+        - if 'DESDM_DEBUG' is an environment variable then it's value is used
+        - if envdbgvar is an environment variable then it's value is used
+        - if the first part of envdbgvar (test before the first '_') + '_DEBUG'
+          is an environment variable then it's value is used. e.g: 'FWMG_TEST'
+          would become 'FWMG_DEBUG'
+        - use 0
+
+        Parameters
+        ----------
+        msglvl : int
+            The debug level to check
+
+        envdbgvar : str
+           The environment variable to check the value of
+
+        Returns
+        -------
+        bool, True if msglvl is less than or equal to the current debug level,
+        False otherwise
+    """
     # environment debug variable overrides code set level
 
     dbglvl = 0
@@ -44,13 +86,37 @@ def fwdebug_check(msglvl, envdbgvar):
 
 #######################################################################
 def fwdebug_print(msgstr, msgprefix=''):
-    """ print debugging message """
+    """ Print the given message to the screen with the given prefix, current
+        system time, and calling file name.
+
+        Parameters
+        ----------
+        msgstr : str
+            The message to print
+
+        msgprefix : str
+            Text to prepend to the output line. Default is an empty string
+
+    """
     print "%s%s - %s - %s" % (msgprefix, datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"),
                               inspect.stack()[1][3], msgstr)
 
 #######################################################################
 def fwdie(msg, exitcode, depth=1):
-    """ abort after printing short message include some info from backtrace """
+    """ Abort with the given exit code, after printing a short message,
+        including some info from backtrace.
+
+        Parameters
+        ----------
+        msg : str
+            The message to print
+
+        exitcode : int
+            The code to exit python with
+
+        depth : int
+            How many line of backtrace to print. Default is 1
+    """
     frame = inspect.stack()[depth]
     fle = os.path.basename(frame[1])
     print "\n\n%s:%s:%s: %s" % (fle, frame[3], frame[2], msg)
@@ -59,7 +125,27 @@ def fwdie(msg, exitcode, depth=1):
 
 #######################################################################
 def fwsplit(fullstr, delim=','):
-    """ Split by delim and trim substrs, expand #:# into range """
+    """ Split the input string by delim and trim substrs, expand #:# into range
+
+        Parameters
+        ----------
+        fullstr : str
+            The string to split
+
+        delim : str
+            The delimiter to use for splitting the string. Default is a comma
+
+        Returns
+        -------
+        List containing the elements from the split
+
+        Examples
+        --------
+        fwsplit('1,2,3,4') returns ['1', '2', '3', '4']
+
+        fwsplit('1,2,4:6') returns ['1', '2', '4', '5', '6']
+
+    """
     #fullstr = re.sub('[()]', '', fullstr) # delete parens if exist
     fullstr = fullstr.replace('(', '')
     fullstr = fullstr.replace(')', '')
@@ -77,7 +163,14 @@ def fwsplit(fullstr, delim=','):
 
 #######################################################################
 def coremakedirs(thedir):
-    """ Call os.makedirs handling path already exists """
+    """ Create a directory, but just return gracefully if it already
+        exists.
+
+        Parameters
+        ----------
+        thedir : str
+            The directory to make
+    """
     if thedir and not os.path.exists(thedir):  # some parallel filesystems really don't like
                                                # trying to make directory if it already exists
         try:
@@ -97,7 +190,36 @@ CU_PARSE_EXTENSION = 1   # deprecating use CU_PARSE_COMPRESSION
 CU_PARSE_COMPRESSION = 1
 CU_PARSE_BASENAME = 0
 def parse_fullname(fullname, retmask=CU_PARSE_FILENAME):
-    """ parse the full name of a file """
+    """ Parse the given file name, returning the requested parts
+
+        Parameters
+        ----------
+        fullname : str
+            The name of the file (can include the path)
+
+        retmask : int
+            The requested return values. Acceptable values are:
+
+            CU_PARSE_BASENAME : return just the file name, including the
+                extension(s), removing any path components
+            CU_PARSE_COMPRESSION : return the compression, valid compression value
+                are '.fz' and '.gz'
+            CU_PARSE_FILENAME : return the filename with any compression extension
+                removed as well as removing any path components
+            CU_PARSE_PATH : return just the directory path to the file
+            CU_PARSE_HDU : return the HDU of the file, this is specified by an int
+                inside of [] at the end of the file. e.g. file.fits[2] has an HDU of 2
+
+            These values can be logically or'd together to get multiple return values.
+            e.g. CU_PARSE_FILENAME | CU_PARSE_COMPRESSION will return both the file name
+            and the compression as a two element list
+
+            Default value is CU_PARSE_FILENAME.
+        Returns
+        -------
+        A single string if only one item was requested, or a list of the requested
+        parts if retmask is an or'd value. None will be returned if there is no result.
+    """
     fwdebug(3, 'MISCUTILS_DEBUG', "fullname = %s" % fullname)
     fwdebug(3, 'MISCUTILS_DEBUG', "retmask = %s" % retmask)
 
@@ -171,7 +293,24 @@ def parse_fullname(fullname, retmask=CU_PARSE_FILENAME):
 
 #######################################################################
 def convertBool(var):
-    """ convert text to a bool"""
+    """ Convert the given item to a bool. If the item type is an int
+        then it is cast to a bool (0 = False, any other value is True).
+        If the item type is a string the 'y', 'yes', and 'true' will return
+        True, and 'n', 'no', and 'false' will return False (case insensitive).
+        If the given string is an int (e.g. '2') then the int value will be
+        cast to a bool. If the item type is a bool, then it's value is returned.
+        If the item is None, then False is returned. Any other data type will
+        raise an exception.
+
+        Parameters
+        ----------
+        var : various
+            The item to convert to a bool
+
+        Returns
+        -------
+        The bool value of the input item
+    """
     #print "Before:", var, type(var)
     newvar = None
     if var is not None:
@@ -201,7 +340,28 @@ def convertBool(var):
 #    Function argument value overrides environment variable
 #    Nothing set defaults to using DB
 def use_db(arg):
-    """ determine if we are using the database or not """
+    """ Determine if we are using the database or not, based on the
+        input dictionary, class, or value, or failing that use the
+        value in the 'DESDM_USE_DB' environment variable. True
+        is returned if none of the baove can be found.
+
+        Parameters
+        ----------
+        arg : varous
+            The item to search for a 'use_db' value. This is done in the following
+            order
+            - If arg is a dictionary, then the value of arg['use_db']
+              is returned
+            - If arg is a class or other structure, then the value of
+              arg.use_db is returned
+            - If arg is any other type then it is cast to a bool and that
+              value is returned.
+
+        Returns
+        -------
+        bool, True if any of the above parameters evaluate to True or all
+        do not exist, False otherwise.
+    """
     use = True
 
     args_use_db = None
@@ -232,7 +392,28 @@ def use_db(arg):
 #    Function argument value overrides environment variable
 #    Lower case key for arg lookup, Upper case for environ lookup
 def checkTrue(key, arg, default=True):
-    """ check whether a value is true"""
+    """ Check whether the given value is true. If arg is a dictionary then
+        the boolean value of arg[key] is returned. If arg is a class or other structure
+        then the boolean value of arg.key is returned. If arg is another type then
+        arg is cast to a bool and that value is returned. Failing that, if DESDM_arg
+        is an environment variable then that is cast to a bool. Otherwise the default
+        value is returned.
+
+        Parameters
+        ----------
+        key : str
+            The dictionary key, or object attribute to look up.
+
+        arg : various
+            The item to search for the value os key in.
+
+        default : bool
+            The default value to return if key is not found. Defaults to True.
+
+        Returns
+        -------
+        The bool value of the requested item, or the given default value.
+    """
     ret_val = default
 
     args_val = None
@@ -263,8 +444,26 @@ def checkTrue(key, arg, default=True):
 ## PrettyPrinter doesn't work for certain nested dictionary (OrderedDict) cases
 ##     http://bugs.python.org/issue10592
 def pretty_print_dict(the_dict, out_file=None, sortit=False, indent=4):
-    """Outputs a given dictionary in a format easier for human reading where items within
-       the same sub-dictionary could be output in alphabetical order"""
+    """ Outputs a given dictionary in a format easier for human reading where items within
+        the same sub-dictionary could be output in alphabetical order
+
+        Parameters
+        ----------
+        the_dict : dict
+            The dictionary to print nicely
+
+        out_file : str
+            The file handle to write the pretty printed dictionary to. Defaults to
+            None, which is sys.stdout.
+
+        sortit : bool
+            Whether to sort the keys at each level alphabetically before printing
+            them. Default is False.
+
+        indent : int
+            The indentation to use, in number of spaces, for each level (additive) of the dictionary
+            as it is printed out. Default is 4.
+    """
     if out_file is None:
         out_file = sys.stdout
     assert the_dict is not None, "Passed in None for dictionary arg"
@@ -292,7 +491,29 @@ def _recurs_pretty_print_dict(the_dict, out_file, sortit, inc_indent, curr_inden
 
 #######################################################################
 def get_config_vals(extra_info, config, keylist):
-    """ Search given dicts for specific values """
+    """ Search the given dictionaries for the given keys. Returning a
+        dictionary of the retrieved values. This will exit python
+        if one of the keys is marked as required, but is not found. If
+        the requested key is found in extra_info, then that value will
+        take precedence over its config value.
+
+        Parameters
+        ----------
+        extra_info : dict
+            Dictionary to search first for each key.
+
+        config : dict
+            Dictionary to search second for each key
+
+        keylist : dict
+            Dictionary containing key-value pairs of the keys to search
+            for and values indicating whther they are required (value of
+            'req', case insensitive)
+
+        Returns
+        -------
+        Dict containing the found keys and their values.
+    """
     info = {}
     for k, stat in keylist.items():
         if extra_info is not None and k in extra_info:
@@ -309,7 +530,17 @@ def get_config_vals(extra_info, config, keylist):
 
 #######################################################################
 def dynamically_load_class(class_desc):
-    """ Loads class at runtime based upon given string description """
+    """ Loads class at runtime based upon given string description
+
+        Paramters
+        ---------
+        class_desc : str
+            The name of the class to load (but not instantiate)
+
+        Returns
+        -------
+        Class of the requested type
+    """
 
     fwdebug(3, 'MISCUTILS_DEBUG', "class_desc = %s" % class_desc)
     modparts = class_desc.split('.')
@@ -323,7 +554,19 @@ def dynamically_load_class(class_desc):
 
 #######################################################################
 def updateOrderedDict(d, u):
-    """ update dictionary recursively to update nested dictionaries """
+    """ Update a dictionary. This is done recursively in the case where
+        the dictionary value(s) is also a dictionary. If any of the input
+        keys do not exist in the dictionary being updated, then they are
+        added to that dictionary.
+
+        Parameters
+        ----------
+        d : dict
+            The dictionary being updated
+
+        u : dict
+            The dictionary containing the new key-value pairs for d.
+    """
 
     for k, v in u.iteritems():
         if isinstance(v, Mapping):
@@ -340,7 +583,18 @@ def updateOrderedDict(d, u):
 
 #######################################################################
 def get_list_directories(filelist):
-    """ get a list of directories from a file list """
+    """ Get a list of directories from a list of files. This is a unique
+        list of all directories in the path of each of the files.
+
+        Parameters
+        ----------
+        filelist : list
+            List of files including their paths
+
+        Returns
+        -------
+        List of directories in the paths of the files.
+    """
     dirlist = {}
     for f in filelist:
         filedir = parse_fullname(f, CU_PARSE_PATH)
@@ -358,7 +612,20 @@ def get_list_directories(filelist):
 # Some functions added by Felipe Menanteau, coming from the old despyutils
 
 def elapsed_time(t1, verbose=False):
-    """ Formating of the elapsed time """
+    """ Return the elapsed time since t1 in a human readable format.
+
+        Paramters
+        ---------
+        t1 : time instance
+            The time from which the calculations are based
+
+        verboase : bool
+            Whether to print out the result to the screen. Default is False
+
+        Returns
+        -------
+        str, representing the ellapsed time since t1.
+    """
     import time
     t2 = time.time()
     stime = "%dm %2.2fs" % (int((t2 - t1) / 60.), (t2 - t1) - 60 * int((t2 - t1) / 60.))
@@ -367,9 +634,21 @@ def elapsed_time(t1, verbose=False):
     return stime
 
 def query2dict_of_lists(query, dbhandle):
-    """
-    Transforms the result of an SQL query and a Database handle object [dhandle]
-    into a dictionary of lists
+    """ Transforms the result of an SQL query and a Database handle object [dhandle]
+        into a dictionary of lists. Each dictionary key is a column and the memebers of
+        the corresponding lists are the row values.
+
+        Parameters
+        ----------
+        query : str
+            The sql query to perform
+
+        dbhandle : handle
+            Handle to the database to operate on
+
+        Returns
+        -------
+        Dict containing the values from the query.
     """
 
     # Get the cursor from the DB handle
@@ -391,7 +670,18 @@ def query2dict_of_lists(query, dbhandle):
 
 
 def create_logger(level=logging.NOTSET, name='default'):
-    """ instantiate a logger """
+    """ Create a logging instance
+
+        Parameters
+        ----------
+        level : int
+            The cutoff logging level (any log messages with levels
+            above this will not be logged). Default is logging.NOTSET,
+            all messages are logged.
+
+        name : str
+            The name of the logger to use. Default is 'default'
+    """
     logging.basicConfig(level=level,
                         format='[%(asctime)s] [%(levelname)s] %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
